cb055880241feb3a7e0f6e45ae0596fa69df8f9d
Ga Ming Chan <gmchan@Gas-MacBook-Pro.local>
2023-04-14 23:44:23 +0100

    added count down

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const start = document.getElementById("start");
+const timer = document.getElementById("time");
 const goomba = document.querySelectorAll(".goomba");
 const mario = document.querySelectorAll(".mario");
 let previous;
+let gameStatus = false;
+let gameOver = false;
 
+// pressing will start the game
 start.addEventListener("click", () => {
   start.style.visibility = "hidden";
+  time();
   gameStart();
 });
+
+const time = () => {
+  let num = 10;
+  gameStatus = true;
+  if (gameStatus) {
+    timer.style.visibility = "initial";
+  }
+  let countDown = setInterval(() => {
+    num--;
+    if (num === 0) {
+      clearInterval(countDown);
+      start.innerHTML = "Try Again?";
+      start.style.visibility = "initial";
+      gameOver = true;
+    }
+    timer.innerHTML = `${num}`;
+  }, 1000);
+};
 
 const gameStart = () => {
   // randomly picks 0 or 1
@@ -16,7 +39,7 @@ const gameStart = () => {
 
   // randomly picks which to pop up and prevents previous number from being picked again
   const randomGoombaMario = () => {
-    let rnd = Math.floor(Math.random() * 3);
+    let rnd = Math.floor(Math.random() * goomba.length);
     if (rnd === previous) {
       return randomGoombaMario();
     }
